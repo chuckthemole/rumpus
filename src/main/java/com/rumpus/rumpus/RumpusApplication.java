ab15b6bb6e726a8e9ec4eef4f22e581fb134e3bd
Charles <cthomas@callistomedia.com>
2022-12-30 19:20:31 -0800

    Init commit

---
+++ src/main/java/com/rumpus/rumpus/RumpusApplication.java
@@ -0,0 +1,13 @@
+package com.rumpus.rumpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumpusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rumpus/rumpus/RumpusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumpus.rumpus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumpusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
