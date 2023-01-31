8275dda56e9ae742c75b23e849419075e36bd604
Charles <cthomas@callistomedia.com>
2023-01-30 16:20:56 -0800

    Move build.gradle central and update runner

--- runner.py
+++ runner.py
@@ -3,12 +3,14 @@
 import os
 import sys
 
+# TODO build this out to download external dependencies (buildSrc/common)
+
 # Commands
-build = "./gradlew clean build --refresh-dependencies -x test > src/main/java/com/rumpus/rumpus/log/build.log"
-buildTest = "./gradlew clean build --refresh-dependencies > src/main/java/com/rumpus/rumpus/log/build.log"
-buildTestInfo = "./gradlew clean build --info --refresh-dependencies > src/main/java/com/rumpus/rumpus/log/build.log"
-buildTestDebug = "./gradlew clean build --debug --refresh-dependencies > src/main/java/com/rumpus/rumpus/log/build.log"
-test = "./gradlew test > src/test/java/com/rumpus/rumpus/log/test.log"
+build = ".././gradlew clean build --refresh-dependencies -x test > src/main/java/com/rumpus/rumpus/log/build.log"
+buildTest = ".././gradlew clean build --refresh-dependencies > src/main/java/com/rumpus/rumpus/log/build.log"
+buildTestInfo = ".././gradlew clean build --info --refresh-dependencies > src/main/java/com/rumpus/rumpus/log/build.log"
+buildTestDebug = ".././gradlew clean build --debug --refresh-dependencies > src/main/java/com/rumpus/rumpus/log/build.log"
+test = ".././gradlew test > src/test/java/com/rumpus/rumpus/log/test.log"
 run = "./gradlew bootRun > src/main/java/com/rumpus/rumpus/log/spring.log"
 
 def commands():
