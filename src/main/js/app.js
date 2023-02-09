781b1eb081ff1083fd08c6a620449f56656378c4
Charles <cthomas@callistomedia.com>
2023-02-09 12:08:47 -0800

    Working signup/login modals

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -5,89 +5,46 @@ const client = require('./client');
 import Users from './users';
 import Footer from './footer';
 import Signup from './signup';
+import Login from './login';
 
 function InitModals() {
+    // Add modal here Map[name, modal]
+    // modal array: index 0 is modal, index 1 is button
     const modals = new Map();
-    // modals.set("login", [
-    //     document.getElementsByClassName("login")[0], 
-    //     document.getElementsByClassName("loginBtn")[0]]
-    // );
     modals.set("signup", [
         document.getElementsByClassName("signup")[0],
         document.getElementsByClassName("signupBtn")[0]]
     );
+    modals.set("login", [
+        document.getElementsByClassName("login")[0],
+        document.getElementsByClassName("loginBtn")[0]]
+    );
 
-    for (let value of modals.values()) {
-        var modal = value[0];
-        var button = value[1];
-        var background = modal.getElementsByClassName("modal-background")[0];
-        var close = modal.getElementsByClassName("modal-close")[0];
-
-        button.onclick = function() {
-            modal.classList.add("is-active");
-            modal.classList.add("is-clipped");
+    // For each modal, add click event
+    // on button click add is-active, on x click remove is-active
+    for(let value of modals.values()) {
+        value[1].onclick = function() {
+            value[0].classList.add("is-active");
+            value[0].classList.add("is-clipped");
         }
-        window.onclick = function(event) {
-            if(event.target == background) {
-                modal.classList.remove("is-active");
-                modal.classList.remove("is-clipped");
-            }
-        }
-        close.onclick = function() {
-            modal.classList.remove("is-active");
-            modal.classList.remove("is-clipped");
+        value[0].getElementsByClassName("modal-close")[0].onclick = function() {
+            value[0].classList.remove("is-active");
+            value[0].classList.remove("is-clipped");
         }
     }
-}
-
-function SignupForm() { // Not using right now
-    // Add JQuery below here
-    $(document).ready(function () {
-        // Sign up form
-        $("#signup-form").submit(function (event) {
-    
-            //stop submit the form event. Do this manually using ajax post function
-            event.preventDefault();
-    
-            var signupForm = {}
-            signupForm["username"] = $("#username").val();
-            signupForm["password"] = $("#email").val();
-            signupForm["password"] = $("#password").val();
-    
-            $("#btn-signup").prop("disabled", true);
-            
-            $.ajax({
-                type: "POST",
-                contentType: "application/json",
-                url: "/api/signup",
-                data: JSON.stringify(signupForm),
-                dataType: 'json',
-                cache: false,
-                timeout: 600000,
-                success: function (data) {
-    
-                    var json = "<h4>Ajax Response</h4><pre>"
-                        + JSON.stringify(data, null, 4) + "</pre>";
-                    $('#feedback').html(json);
-    
-                    console.log("SUCCESS : ", data);
-                    $("#btn-signup").prop("disabled", false);
-    
-                },
-                error: function (e) {
-    
-                    var json = "<h4>Ajax Response Error</h4><pre>"
-                        + e.responseText + "</pre>";
-                    $('#feedback').html(json);
-    
-                    console.log("ERROR : ", e);
-                    $("#btn-signup").prop("disabled", false);
-    
-                }
-            });
-            
+    // If click outside the modal, close modal
+    window.onclick = function(event) {
+        var backgrounds = document.getElementsByClassName("modal-background");
+        Array.prototype.forEach.call(backgrounds, function(background) { // get all modal backgrounds
+            if(event.target == background) {
+                var modals = document.getElementsByClassName("modal");
+                Array.prototype.forEach.call(modals, function(modal) { // remove is-active from all modals
+                    modal.classList.remove("is-active");
+                    modal.classList.remove("is-clipped");
+                });
+            }
         });
-    });
+    }
 }
 
 export default function App() {
@@ -99,6 +56,9 @@ export default function App() {
 
     const signup = ReactDOM.createRoot(document.getElementById('signup-form'));
     signup.render(<Signup />);
+
+    const login = ReactDOM.createRoot(document.getElementById('login-form'));
+    login.render(<Login />);
 
     InitModals();
 }
--- src/main/js/login.js
+++ src/main/js/login.js
@@ -2,34 +2,25 @@ const React = require('react');
 const ReactDOM = require('react-dom/client');
 const client = require('./client');
 
-class Signup extends React.Component {
+class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {username: '', email: '', password: ''};
+        this.state = {username: '', password: ''};
 
         this.handleUsername = this.handleUsername.bind(this);
-        this.handleEmail = this.handleEmail.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
-        // this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
-    // handleChange(event) {
-    //   this.setState({value: event.target.value});
-    // }
 
     handleUsername(event) {
         this.setState({username: event.target.value});
-    }
-    handleEmail(event) {
-        this.setState({email: event.target.value});
     }
     handlePassword(event) {
         this.setState({password: event.target.value});
     }
   
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.username + ' ' + this.state.email + ' ' + this.state.password);
+      alert('A name was submitted: ' + this.state.username + ' ' + this.state.password);
       event.preventDefault();
     }
   
@@ -47,15 +38,6 @@ class Signup extends React.Component {
                     </div>
                 </div>
                 <div className="field">
-                    <label htmlFor="" className="label">Email</label>
-                    <div className="control has-icons-left">
-                        <input type="email" placeholder="e.g. bobsmith@gmail.com" className="input" value={this.state.email} onChange={this.handleEmail} required />
-                            <span className="icon is-small is-left">
-                                <i className="fa fa-envelope"></i>
-                            </span>
-                    </div>
-                </div>
-                <div className="field">
                     <label htmlFor="" className="label">Password</label>
                     <div className="control has-icons-left">
                         <input type="password" placeholder="*******" className="input" value={this.state.password} onChange={this.handlePassword} required />
@@ -64,17 +46,8 @@ class Signup extends React.Component {
                         </span>
                     </div>
                 </div>
-                {/* <!-- <div className="field">
-                    <label htmlFor="" className="label">Confirm Password</label>
-                    <div className="control has-icons-left">
-                        <input type="confirm_password" placeholder="*******" className="input" required>
-                        <span className="icon is-small is-left">
-                            <i className="fa fa-lock"></i>
-                        </span>
-                    </div>
-                </div> --> */}
                 <div className="field">
-                    <button type="submit" value="Signup" className="button is-success">
+                    <button type="submit" value="Login" className="button is-success">
                         Submit
                     </button>
                 </div>
@@ -84,4 +57,4 @@ class Signup extends React.Component {
     }
 }
   
-export default Signup;
+export default Login;
