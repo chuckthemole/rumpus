5fbb1a430e81b62b6502a774800941c372521dfa
Chuck Thomas <chuckthemole@gmail.com>
2023-07-28 14:06:26 -0700

    Delete common folder, to be added to gitignore, create webpack for common

--- src/main/js/common/logout.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-const logout = <form method="post" action="/logout"><button className="logoutBtn button is-danger" type="submit" value="Sign Out">Sign Out</button></form>;
-
-export default function Logout() {
-    return logout;
-}
