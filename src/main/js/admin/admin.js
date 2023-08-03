721f62a2b5e62174df33fedd7d891548f6df2de9
Chuck Thomas <chuckthemole@gmail.com>
2023-08-03 13:25:27 -0700

    Alter package.json for mkdir of common if doesnt exist, clean up some front end on admin

--- src/main/js/admin/admin.js
+++ src/main/js/admin/admin.js
@@ -24,36 +24,32 @@ export default function Admin() {
 
     return (
         <>
-            <div className='columns'>
-                <div className='column is-four-fifths'>
-                    <div className="tabs is-boxed">
-                        <ul>
-                            <li className={`adminTab ${homeActive && is_active}`}>
-                                <a onClick={ ()=> { clear(); setHome(true); setActiveWindow(<AdminHome />); } }>
-                                    <span>Admin Home</span>
-                                </a>
-                            </li>
-                            <li className={`usersTab ${usersActive && is_active}`}>
-                                <a onClick={ ()=> { clear(); setUsers(true); setActiveWindow(<Users />); } }>
-                                    <span>Users</span>
-                                </a>
-                            </li>
-                            <li className={`temp1Tab ${temp1Active && is_active}`}>
-                                <a onClick={ ()=> { clear(); setTemp1(true); setActiveWindow(<div><span>Temp1</span></div>); } }>
-                                    <span>I Need</span>
-                                </a>
-                            </li>
-                            <li className={`temp2Tab ${temp2Active && is_active}`}>
-                                <a onClick={ ()=> { clear(); setTemp2(true); setActiveWindow(<div><span>Temp2</span></div>); } }>
-                                    <span>More Tabs</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='is-centered has-background-light box'>{activeWindow}</div>
-                </div>
+            <div className="tabs is-boxed">
+                <ul>
+                    <li className={`adminTab ${homeActive && is_active}`}>
+                        <a onClick={ ()=> { clear(); setHome(true); setActiveWindow(<AdminHome />); } }>
+                            <span>Admin Home</span>
+                        </a>
+                    </li>
+                    <li className={`usersTab ${usersActive && is_active}`}>
+                        <a onClick={ ()=> { clear(); setUsers(true); setActiveWindow(<Users />); } }>
+                            <span>Users</span>
+                        </a>
+                    </li>
+                    <li className={`temp1Tab ${temp1Active && is_active}`}>
+                        <a onClick={ ()=> { clear(); setTemp1(true); setActiveWindow(<div><span>Temp1</span></div>); } }>
+                            <span>I Need</span>
+                        </a>
+                    </li>
+                    <li className={`temp2Tab ${temp2Active && is_active}`}>
+                        <a onClick={ ()=> { clear(); setTemp2(true); setActiveWindow(<div><span>Temp2</span></div>); } }>
+                            <span>More Tabs</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
+
+            <div className='is-centered has-background-light box'>{activeWindow}</div>
         </>
     )
 }
