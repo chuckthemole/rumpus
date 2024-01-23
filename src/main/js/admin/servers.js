175cee35889fc7a1d99b4f01255f64437035aef2
Chuck Thomas <chuckthemole@gmail.com>
2024-01-22 16:49:14 -0800

    Fix servers.js useEffect, so it is not making too many calls

--- src/main/js/admin/servers.js
+++ src/main/js/admin/servers.js
@@ -1,164 +1,145 @@
-const React = require('react');
+import React, { useEffect, useState } from 'react';
 const ReactDOM = require('react-dom/client');
 
+import useSWR from 'swr';
+import { common_fetcher } from '../common/common_requests';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { Common, DELETE_USER_PATH, GET_USERS_PATH, CREATE_USER_PATH, GET_SERVERS_PATH, START_SERVER_PATH, STOP_SERVER_PATH } from "../rumpus";
 import { useLoaderData, Link, useFetcher } from 'react-router-dom';
 import ServerAction, { StartServer } from './server_action';
-
-// sort flags
-const sort_by_username = '/username';
-const sort_by_email = '/email';
-const sort_by_id = '/id';
-// keeps track of the current sort
-let current_sort;
-
-export async function loader() {
-    const response = await fetch(GET_SERVERS_PATH);
-    // If the status code is not in the range 200-299,
-    // we still try to parse and throw it.
-    if (!response.ok) {
-        const error = new Error('An error occurred while fetching servers.');
-        // Attach extra info to the error object.
-        error.info = await response.json();
-        error.status = response.status;
-        throw error;
-    }
-    if(response.redirected == true) { // catching this and returning null as to not get console error
-        return null;
-    }
-
-    return response.json();
-}
+import { RenderFromSWR } from '../common/error_page';
 
 export async function delete_server(server_name) {
-
 }
 
+/**
+ * This component is responsible for displaying servers. TODO: not really using too much right now. Look into this more later. -chuck 2024/1/22
+ * @returns the servers page in table view
+ */
 export default function Servers() {
-
-    const [servers, setServers] = React.useState(useLoaderData());
-    console.log(servers);
     
-    React.useEffect(() => { // TODO: this calls the api a lot. figure out a resolution.
-        loader().then((response) => {
-            setServers(response);
-        });
-    }, [servers]); // TODO: can i use effect when button pressed?
+    const [servers, setServers] = useState(useLoaderData());
+    const [render_content, setRenderContent] = useState(<></>);
+    const {data, error} = useSWR(
+        GET_SERVERS_PATH,
+        common_fetcher
+    );
+    
+    // check if data is loaded then set servers
+    useEffect(() => {
+        if(data !== undefined && data !== null && data !== '') {
+            // console.log(data);
+            setServers(data);
+        }
+    }, [data]);
 
-    if (!servers) return(
-        <div className='container m-6'>
-            <progress className="progress is-small is-primary" max="100">15%</progress>
-            <progress className="progress is-danger" max="100">30%</progress>
-            <progress className="progress is-medium is-dark" max="100">45%</progress>
-            <progress className="progress is-large is-info" max="100">60%</progress>
-        </div>
-    )
+    // check if servers are set then set render_content
+    useEffect(() => {
+        if(servers !== undefined && servers !== null && servers !== '') {
+            setRenderContent(
+                <>
+                    <div className='table-container'>
+                        <table className="table is-hoverable is-fullwidth is-bordered m-6">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>
+                                        Server
+                                    </th>
+                                    <th>
+                                        Start
+                                    </th>
+                                    <th>
+                                        Stop
+                                    </th>
+                                    <th>
+                                        Running
+                                    </th>
+                                    <th>
+                                        Host
+                                    </th>
+                                    <th>
+                                        Port
+                                    </th>
+                                    <th>
+                                        View
+                                    </th>
+                                    <th>
+                                        Delete
+                                    </th>
+                                    <th>
+                                        Update
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>#</th>
+                                    <th>
+                                        Server
+                                    </th>
+                                    <th>
+                                        Start
+                                    </th>
+                                    <th>
+                                        Stop
+                                    </th>
+                                    <th>
+                                        Running
+                                    </th>
+                                    <th>
+                                        Host
+                                    </th>
+                                    <th>
+                                        Port
+                                    </th>
+                                    <th>
+                                        View
+                                    </th>
+                                    <th>
+                                        Delete
+                                    </th>
+                                    <th>
+                                        Update
+                                    </th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                {servers.map(( server, index ) => (
+                                    <tr key={index}>
+                                        <th>{index + 1}</th>
+                                        <td>{server.serverName}</td>
+                                        <td>{server.isRunning ? <button className="button is-success is-small" disabled>Start</button> : <button onClick={() => fetch(START_SERVER_PATH + server.serverName)} className="button is-success is-small">Start</button>}</td>
+                                        <td>{!server.isRunning ? <button className="button is-danger is-small" disabled>Stop</button> : <button onClick={() => fetch(STOP_SERVER_PATH + server.serverName)} className="button is-danger is-small">Stop</button>}</td>   
+                                        {/* <td>
+                                            {server.isRunning ? <ServerAction disabled server={server} action='start_server' /> : <ServerAction disabled server={server} action='start_server' />}
+                                        </td>
+                                        <td>
+                                            {server.isRunning ? <ServerAction server={server} action='stop_server' /> : <ServerAction server={server} action='stop_server' />}
+                                        </td> */}
+                                        <td>{server.isRunning ? "TRUE" : "FALSE"}</td>
+                                        <td>{server.hostIp}</td>
+                                        <td>{server.port}</td>
+                                        {/* <td title={ConvertEpochToDate(user.metaData.creationTime).toString()}>{ConvertEpochToDate(user.metaData.creationTime).toDateString()}</td> */}
+                                        <td>
+                                            todo
+                                        </td>
+                                        <td>
+                                            todo
+                                        </td>
+                                        <td>
+                                            todo
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </>
+            )
+        }
+    }, [servers]);
 
-    return (
-        <>
-            <div className='table-container'>
-                <table className="table is-hoverable is-fullwidth is-bordered m-6">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>
-                                Server
-                            </th>
-                            <th>
-                                Start
-                            </th>
-                            <th>
-                                Stop
-                            </th>
-                            <th>
-                                Running
-                            </th>
-                            <th>
-                                Host
-                            </th>
-                            <th>
-                                Port
-                            </th>
-                            <th>
-                                View
-                            </th>
-                            <th>
-                                Delete
-                            </th>
-                            <th>
-                                Update
-                            </th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th>#</th>
-                            <th>
-                                Server
-                            </th>
-                            <th>
-                                Start
-                            </th>
-                            <th>
-                                Stop
-                            </th>
-                            <th>
-                                Running
-                            </th>
-                            <th>
-                                Host
-                            </th>
-                            <th>
-                                Port
-                            </th>
-                            <th>
-                                View
-                            </th>
-                            <th>
-                                Delete
-                            </th>
-                            <th>
-                                Update
-                            </th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                        {servers.map(( server, index ) => (
-                            <tr key={server.serverName}>
-                                <th>{index + 1}</th>
-                                <td>{server.serverName}</td>
-                                <td>{server.isRunning ? <button className="button is-success is-small" disabled>Start</button> : <button onClick={() => fetch(START_SERVER_PATH + server.serverName)} className="button is-success is-small">Start</button>}</td>
-                                <td>{!server.isRunning ? <button className="button is-danger is-small" disabled>Stop</button> : <button onClick={() => fetch(STOP_SERVER_PATH + server.serverName)} className="button is-danger is-small">Stop</button>}</td>   
-                                {/* <td>
-                                    {server.isRunning ? <ServerAction disabled server={server} action='start_server' /> : <ServerAction disabled server={server} action='start_server' />}
-                                </td>
-                                <td>
-                                    {server.isRunning ? <ServerAction server={server} action='stop_server' /> : <ServerAction server={server} action='stop_server' />}
-                                </td> */}
-                                <td>{server.isRunning ? "TRUE" : "FALSE"}</td>
-                                <td>{server.hostIp}</td>
-                                <td>{server.port}</td>
-                                {/* <td title={ConvertEpochToDate(user.metaData.creationTime).toString()}>{ConvertEpochToDate(user.metaData.creationTime).toDateString()}</td> */}
-                                <td>
-                                    todo
-                                </td>
-                                <td>
-                                    todo
-                                </td>
-                                <td>
-                                    todo
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className='container m-4'>
-                todo
-            </div>
-
-        </>
-    )
+    return RenderFromSWR(data, error, render_content, 'An error occurred getting servers.');
 }
