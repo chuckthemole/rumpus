5003156b12b0de9d2160015bf519500b10010caf
Chuck Thomas <chuckthemole@gmail.com>
2024-02-02 11:33:34 -0800

    Refactor AbstractServerPortCustomizer package

--- src/main/java/com/rumpus/rumpus/config/RumpusPortCustomizer.java
+++ src/main/java/com/rumpus/rumpus/config/RumpusPortCustomizer.java
@@ -1,7 +1,7 @@
 package com.rumpus.rumpus.config;
 
 @org.springframework.stereotype.Component
-public class RumpusPortCustomizer extends com.rumpus.common.Config.AbstractServerPortCustomizer {
+public class RumpusPortCustomizer extends com.rumpus.common.Server.AbstractServerPortCustomizer {
  
     private static final String NAME = "RumpusPortCustomizer";
     private static final String PORT = "8081"; // TODO: look in AbstractServerPortCustomizer for its todo. I should make a port manager, or look for one if I started one earlier.
