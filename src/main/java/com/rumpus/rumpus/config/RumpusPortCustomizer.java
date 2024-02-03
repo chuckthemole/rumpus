57e6ee05b2cb17136ef7fc426d01c24568a5d404
Chuck Thomas <chuckthemole@gmail.com>
2024-02-03 12:46:56 -0800

    Remove annotations and bring to super class

--- src/main/java/com/rumpus/rumpus/config/RumpusPortCustomizer.java
+++ src/main/java/com/rumpus/rumpus/config/RumpusPortCustomizer.java
@@ -1,12 +1,11 @@
 package com.rumpus.rumpus.config;
 
-@org.springframework.stereotype.Component
-public class RumpusPortCustomizer extends com.rumpus.common.Server.AbstractServerPortCustomizer {
+public class RumpusPortCustomizer extends com.rumpus.common.Config.AbstractServerPortCustomizer {
  
     private static final String NAME = "RumpusPortCustomizer";
     private static final String PORT = "8081"; // TODO: look in AbstractServerPortCustomizer for its todo. I should make a port manager, or look for one if I started one earlier.
 
     public RumpusPortCustomizer() {
-        super(NAME, PORT);
+        super(NAME, RumpusPortCustomizer.PORT, true);
     }
 }
