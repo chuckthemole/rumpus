9ce0cf2fa4180028b4b3603e913d4961b60d4444
Chuck Thomas <chuckthemole@gmail.com>
2024-08-29 12:49:50 -0700

    Move JdbcHttpSessionConfig over to common repo

--- src/main/java/com/rumpus/rumpus/config/JdbcHttpSessionConfig.java
+++ src/main/java/com/rumpus/rumpus/config/JdbcHttpSessionConfig.java
@@ -1,46 +1,12 @@
 package com.rumpus.rumpus.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.session.jdbc.config.annotation.web.http.JdbcHttpSessionConfiguration;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-import org.springframework.transaction.PlatformTransactionManager;
 
-// TODO: Need to look at Sessions more closely and plan how to implement.
+import com.rumpus.common.Config.AbstractJdbcHttpSessionConfig;
 
-// @Configuration(proxyBeanMethods = false)
 @Configuration
-@EnableJdbcHttpSession
-@org.springframework.boot.context.properties.ConfigurationProperties(prefix = "properties")
-@org.springframework.context.annotation.PropertySource(value = "classpath:properties.yml", factory = com.rumpus.common.Config.Properties.YamlPropertySourceFactory.class)
-public class JdbcHttpSessionConfig extends AbstractHttpSessionApplicationInitializer {
-
-    private static final String TABLE_NAME = "session";
-    
-    @Autowired
-	public DataSource dataSource;
-
+public class JdbcHttpSessionConfig extends AbstractJdbcHttpSessionConfig {
     public JdbcHttpSessionConfig() {
-        super(JdbcHttpSessionConfiguration.class);
+        super();
     }
-
-    @Bean
-    @DependsOn({"dataSource"})
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    // Not using rn
-    // @Bean
-    // @DependsOn({"transactionManager"})
-    // public TransactionTemplate transactionTemplate(PlatformTransactionManager manager) {
-    //     return new TransactionTemplate(manager);
-    // }
  }
