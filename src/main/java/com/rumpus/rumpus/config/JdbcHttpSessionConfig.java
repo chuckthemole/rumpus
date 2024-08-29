b17a2c9985deea1aece6e57fcd21627a7fc122c7
Chuck Thomas <chuckthemole@gmail.com>
2024-08-29 14:31:49 -0700

    Abstract JdbcHttpSessionConfig to common repo

--- src/main/java/com/rumpus/rumpus/config/JdbcHttpSessionConfig.java
+++ src/main/java/com/rumpus/rumpus/config/JdbcHttpSessionConfig.java
@@ -1,11 +1,7 @@
 package com.rumpus.rumpus.config;
 
-import org.springframework.context.annotation.Configuration;
-
-import com.rumpus.common.Config.AbstractJdbcHttpSessionConfig;
-
-@Configuration
-public class JdbcHttpSessionConfig extends AbstractJdbcHttpSessionConfig {
+@org.springframework.context.annotation.Configuration
+public class JdbcHttpSessionConfig extends com.rumpus.common.Config.AbstractJdbcHttpSessionConfig {
     public JdbcHttpSessionConfig() {
         super();
     }
--- src/test/java/com/rumpus/rumpus/config/JdbcHttpSessionTestConfig.java
+++ src/test/java/com/rumpus/rumpus/config/JdbcHttpSessionTestConfig.java
@@ -1,42 +1,8 @@
 package com.rumpus.rumpus.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.session.jdbc.config.annotation.web.http.JdbcHttpSessionConfiguration;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-import org.springframework.transaction.PlatformTransactionManager;
-
-@TestConfiguration
-@EnableJdbcHttpSession
-@org.springframework.boot.context.properties.ConfigurationProperties(prefix = "properties")
-@org.springframework.context.annotation.PropertySource(value = "classpath:properties.yml", factory = com.rumpus.common.Config.Properties.YamlPropertySourceFactory.class)
-public class JdbcHttpSessionTestConfig extends AbstractHttpSessionApplicationInitializer {
-    private static final String TABLE_NAME = "session";
-    
-    @Autowired
-	public DataSource dataSource;
-
+@org.springframework.boot.test.context.TestConfiguration
+public class JdbcHttpSessionTestConfig extends com.rumpus.common.Config.AbstractJdbcHttpSessionConfig {
     public JdbcHttpSessionTestConfig() {
-        super(JdbcHttpSessionConfiguration.class);
+        super();
     }
-
-    @Bean
-    @DependsOn({"dataSource"})
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    // Not using rn
-    // @Bean
-    // @DependsOn({"transactionManager"})
-    // public TransactionTemplate transactionTemplate(PlatformTransactionManager manager) {
-    //     return new TransactionTemplate(manager);
-    // }
 }
