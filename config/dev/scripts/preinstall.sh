467441ad09a52a64ee88018fa78799f2730365d8
Chuck Thomas <chuckthemole@gmail.com>
2024-08-29 12:48:54 -0700

    Remove python from preinstall

--- config/dev/scripts/preinstall.sh
+++ config/dev/scripts/preinstall.sh
@@ -4,7 +4,7 @@ rm -f ./src/main/js/common/*
 
 # make venv, activate, and install requirements
 # TODO: look again at these commands and leave comments on each. Also, this is being executed during the build. Let's comment on this, giving good reasons why we're doing this. Same with postinstall.sh
-mkdir -p ./../rumpusenv
-python3 -m venv ./../rumpusenv
-source ./../rumpusenv/bin/activate
-pip install -r ./config/dev/requirements.txt
+# mkdir -p ./../rumpusenv
+# python3 -m venv ./../rumpusenv
+# source ./../rumpusenv/bin/activate
+# pip install -r ./config/dev/requirements.txt
