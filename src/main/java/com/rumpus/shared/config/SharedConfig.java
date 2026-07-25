010f244cf50167d534d0c348e4090b04067183c5
Chuck Thomas <chuckthemole@gmail.com>
2026-07-25 10:19:25 -0400

    Refactor config port and index filter

--- src/main/java/com/rumpus/shared/config/SharedConfig.java
+++ src/main/java/com/rumpus/shared/config/SharedConfig.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import com.rumpus.common.Config.CommonJdbcHttpSessionConfig;
+import com.rumpus.common.Config.Server.ServerPortConfig;
+import com.rumpus.common.Config.Server.ServerConfig;
+import com.rumpus.common.Config.Session.CommonJdbcHttpSessionConfig;
 import com.rumpus.common.Security.Authentication.AuthenticationChecker;
 
 @Configuration
 @Import({
-        CommonJdbcHttpSessionConfig.class
+        CommonJdbcHttpSessionConfig.class,
+        ServerConfig.class,
+        ServerPortConfig.class
 })
 public class SharedConfig {
 
