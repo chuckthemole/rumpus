810ee0bf5e06928997ec0f09fe3d2399a48609c5
Chuck Thomas <chuckthemole@gmail.com>
2026-07-25 09:05:08 -0400

    Refactor of common jdbc http session config

--- src/main/java/com/rumpus/shared/config/SharedConfig.java
+++ src/main/java/com/rumpus/shared/config/SharedConfig.java
@@ -2,10 +2,15 @@ package com.rumpus.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import com.rumpus.common.Config.CommonJdbcHttpSessionConfig;
 import com.rumpus.common.Security.Authentication.AuthenticationChecker;
 
 @Configuration
+@Import({
+        CommonJdbcHttpSessionConfig.class
+})
 public class SharedConfig {
 
     @Bean
